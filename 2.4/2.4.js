4b5a5280b05212e98c5e2e13a4c36fe538fb8ce4
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-08 16:04:20 +0200

    ajout de even/odd pour mieux voir le résultat

--- 2.4/2.4.js
+++ 2.4/2.4.js
@@ -1,9 +1,9 @@
 for (let i=1;i<=100; i++) {
     if (i%2==0){
-        console.log(i/2);
+        console.log('even ' + i/2);
     }
         else {
-            console.log(i*3)
+            console.log('odd ' +i*3)
         }
     
 }
