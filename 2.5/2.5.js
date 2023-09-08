99eac8f9e6761302e29417ab8fe59ea2cef7fff1
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-08 16:53:39 +0200

    fin 2.5

--- 2.5/2.5.js
+++ 2.5/2.5.js
@@ -1,11 +1,13 @@
-let userfavoritenbre= prompt("Entre ton nombre préféré")
-while (userfavoritenbre !== "42"){
-    prompt("Tu es certain? Entre ton VRAI nombre préféré")
-    if (userfavoritenbre == "42") break;
-}
+// let userfavoritenbre= prompt("Entre ton nombre préféré")
+// while (userfavoritenbre != "42"){
+//     prompt("Tu es certain? Entre ton VRAI nombre préféré")
+//     if (userfavoritenbre == "42") break;
+// }
+
 
 // AVEC DO ... WHILE
-// // let userfavoritenbre= prompt("Entre ton nombre préféré")
-// // do{
-// //     prompt("Tu es certain? Entre ton VRAI nombre préféré");
-// // } while (userfavoritenbre == 42 );
+// let userfavoritenbre= prompt("Entre ton nombre préféré")
+do{
+    userfavoritenbre= prompt("Entre ton nombre préféré")
+    alert("Tu es certain?");
+} while (userfavoritenbre != "42" );
