ac825f6700030344633beecfe2e59a0683e18978
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-08 15:42:43 +0200

    début ex bonus du 2.3

--- 2.3/2.3.js
+++ 2.3/2.3.js
@@ -4,3 +4,10 @@ for (let i = 0; i <= 100; i++) {
     }
 }
 
+// // BONUS WHILE  j'arrive pas....
+
+// let nbre = 0
+// while ((nbre<=100) && (nbre%2==0)){
+//    nbre++;
+// }
+// console.log(nbre)
