df5a4cec98ed3967a5220282407abaf720373846
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-08 12:03:24 +0200

    fin exercices 1.0 1.1 et 1.2

---
+++ 1.1/1.1.js
@@ -0,0 +1,9 @@
+let age=25
+console.log(age)
+age=18
+console.log(age)
+
+let phrase="il est 11h50 c'est bientot la pause de midi"
+console.log(phrase)
+phrase="cette nouvelle pahrase va remplacer l'autre "
+console.log(phrase)
---
+++ 1.2/1.2.js
@@ -0,0 +1,5 @@
+let firstName="Gérard"
+let name="Lambert"
+let city= "Paris"
+
+console.log("Your name is " + firstName + " " + name + " and you live in " + city)
