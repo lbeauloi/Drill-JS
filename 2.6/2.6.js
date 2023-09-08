67452a0c679b416ef4b8f0e4ee7080531b295de3
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-08 17:01:54 +0200

    début 2.6

---
+++ 2.6/2.6.js
@@ -0,0 +1,10 @@
+
+let dayoftheweek= prompt(choisi un nombre entre 1 et 7)
+if dayoftheweek ==1 
+console.log("monday")
+
+if dayoftheweek==2
+console.log("tuesday")
+// etc etc etc etc je crois
+
+
