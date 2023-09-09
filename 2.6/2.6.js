5bd18e1b948004d7a812629787280473941e170d
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-09 19:44:40 +0200

    exo 2.6

--- 2.6/2.6.js
+++ 2.6/2.6.js
@@ -1,10 +1,32 @@
 
-let dayoftheweek= prompt(choisi un nombre entre 1 et 7)
-if dayoftheweek ==1 
-console.log("monday")
+let dayoftheweek= prompt("choisi un chiffre entre 1 et 7")
+if (dayoftheweek ==1) {
+    alert("Monday");
+} 
 
-if dayoftheweek==2
-console.log("tuesday")
-// etc etc etc etc je crois
+if (dayoftheweek ==2) {
+    alert("Tuesday");
+} 
+
+if (dayoftheweek ==3) {
+    alert("Wednesday");
+} 
+
+if (dayoftheweek ==4) {
+    alert("Thursday");
+} 
+
+if (dayoftheweek ==5) {
+    alert("Friday");
+} 
+
+if (dayoftheweek ==6) {
+    alert("Saturday");
+} 
+
+if (dayoftheweek ==7) {
+    alert("Sunday");
+} 
+
 
 
